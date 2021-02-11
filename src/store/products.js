24a4a3b59e46c3e03a6184944712cb7420da9dbc
Chris-Bortel <bortelcc19@gmail.com>
2021-02-11 15:54:15 -0800

    Fixed the 500 internal error. I was using quotes instead of backticks

--- src/store/products.js
+++ src/store/products.js
@@ -23,11 +23,13 @@ export default function reducer(state=initialState, action) {
         }
         
 export const getProducts = () => async dispatch => {
+  //TODO: This is causing the 500 error
   let products = await axios({
     method: 'GET',
     // eslint-disable-next-line
-    url: 'https://auth-server-cb.herokuapp.com/api/v1/products'
+    url: `https://auth-server-cb.herokuapp.com/api/v1/products`
   }); 
+  console.log('Just the DATA: ', products.data)
   dispatch ({
     type: 'GETPRODUCTS',
     payload: products.data.results
@@ -38,13 +40,14 @@ export const getProducts = () => async dispatch => {
 
 // Action that will trigger the reducer to reduce the stock counter
 export const decrementStock = (payload) => async dispatch => {
+// TODO: The _id seems to be the issue with the server 500 error. I am recieving the stringValue: ""payload._id""
 console.log('payload in stock:', payload.inStock)
   payload.inStock = payload.inStock-=1;
 
   await axios ({
     method: 'PUT',
-    // eslint-disable-next-line
-    url: 'https://auth-server-cb.herokuapp.com/api/v1/products/${payload._id}',
+  
+    url: `https://auth-server-cb.herokuapp.com/api/v1/products/${payload._id}`,
     data: payload
   })
 
@@ -55,12 +58,14 @@ export const putStockBack = (payload) => async dispatch => {
   let amount = payload.count;
   console.log('new amount: ', amount)
   let newObj = payload.obj;
+  console.log('newObj: ', newObj )
   newObj.inStock = newObj.inStock+amount;
-  console.log('newObj: ', newObj.inStock )
+  console.log('newObj inStock: ', newObj.inStock )
+  //TODO: This is causing the 500 error
   await axios ({
     method: 'PUT',
-    // eslint-disable-next-line
-    url: 'https://auth-server-cb.herokuapp.com/api/v1/products/${newObj._id}',
+  
+    url: `https://auth-server-cb.herokuapp.com/api/v1/products/${newObj._id}`,
     data: newObj
   })
 
